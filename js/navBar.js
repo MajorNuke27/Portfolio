f08cf8cda1948cd9fc398b1d8a03a877b06937a4
Esau <montiel.orihuela.esau@gmail.com>
2023-03-23 15:21:21 -0600

    Implementation of modal navigation menu on tablets and mobiles

--- js/navBar.js
+++ js/navBar.js
@@ -1,4 +1,7 @@
 const navMenu = document.getElementById('nav-menu');
+const btToggleMenu = document.getElementById('toggle-menu');
+const navList = document.getElementsByClassName('nav-bar-list')[0];
+const mediaQuery = window.matchMedia('(max-width: 800px)');
 
 let prevYPos = window.scrollY;
 
@@ -16,3 +19,18 @@ window.onscroll = () => {
 
     prevYPos = currentYPos;
 };
+
+mediaQuery.addEventListener('change', () => {
+    if(mediaQuery.matches) {
+        navList.setAttribute('aria-hidden', true);
+    }
+})
+
+btToggleMenu.onclick = (evt) => {
+    btToggleMenu.classList.toggle('opened');
+    navList.classList.toggle('show-nav-menu');
+
+    btToggleMenu.setAttribute('aria-expanded', btToggleMenu.classList.contains('opened'));
+    navList.setAttribute('aria-hidden', !navList.classList.contains('show-nav-menu'));
+}
+
