3aaf68883017938e2d1b342d11005c2d616d071c
Esau <montiel.orihuela.esau@gmail.com>
2023-03-24 14:11:13 -0600

    Implementation of a go to top button instead of depend on the nav bar logo

--- js/navBar.js
+++ js/navBar.js
@@ -2,29 +2,45 @@ const navMenu = document.getElementById('nav-menu');
 const btToggleMenu = document.getElementById('toggle-menu');
 const navList = document.getElementsByClassName('nav-bar-list')[0];
 const mediaQuery = window.matchMedia('(max-width: 800px)');
+const topBt = document.getElementById('top');
 
 let prevYPos = window.scrollY;
 
 window.onscroll = () => {
+    // Handle nav bar
     var currentYPos = window.scrollY;
 
     if(prevYPos < currentYPos) {
         navMenu.ariaHidden = "true"
-        navMenu.classList.replace('show-nav', 'hide-nav')
+        navMenu.classList.replace('show-nav', 'hide-nav');
     }
     else {
         navMenu.ariaHidden = "false"
-        navMenu.classList.replace('hide-nav', 'show-nav')
+        navMenu.classList.replace('hide-nav', 'show-nav');
     }
 
     prevYPos = currentYPos;
+
+    //Handle go to top link
+    if(window.scrollY >= 100) {
+        topBt.classList.add('show-top');
+        topBt.ariaHidden = false;
+    }
+    else {
+        topBt.classList.remove('show-top');
+        topBt.ariaHidden = true;
+    }
+
 };
 
 mediaQuery.addEventListener('change', () => {
-    if(mediaQuery.matches) {
-        navList.setAttribute('aria-hidden', true);
+    if(mediaQuery.matches){
+        navList.setAttribute('aria-hidden', !navList.classList.contains('show-nav-menu'));
     }
-})
+    else {
+        navList.setAttribute('aria-hidden', false);
+    }
+});
 
 btToggleMenu.onclick = (evt) => {
     btToggleMenu.classList.toggle('opened');
